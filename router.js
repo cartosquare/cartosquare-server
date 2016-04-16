50be079439cd0c382dd13d6ad4e04e36451345fc
XuXiang <xux@geohey.com>
2016-04-16 17:46:37 +0800

    remove log4js

--- router.js
+++ router.js
@@ -3,18 +3,13 @@ var options = require('./config/settings.js').options;
 
 var statistic = require('./statistic.js');
 
-// log
-var log4js = require('log4js');
-log4js.configure(__dirname + '/config/log4js.json');
-var logger = log4js.getLogger('app');
-
 // REST服务
 var restify = require('restify');
 
 var server = restify.createServer();
 
 exports.startServer = function(port, callback) {
-    logger.info('listening on port ' + (port ? port : options.port));
+    console.log('listening on port ' + (port ? port : options.port));
     server.listen(port ? port : options.port, callback);
 };
 
